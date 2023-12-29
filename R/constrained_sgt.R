00067321babcf2993f46610d1651c732b7e3313a
Trinh Dong <trinhdong_0629@outlook.com>
2023-12-29 04:43:58 +0000

    Revert "hello 2024"
    This reverts commit e5d0b7e5f99efc6c0caaea52ef77358f4f765a02.

--- R/constrained_sgt.R
+++ R/constrained_sgt.R
@@ -1,13 +1,13 @@
 #' @rdname sgt
 #' @param link_pq (string, default: logm1) link function for pq = p*q, must be > 1.
 #' @export
-constrained_sgt <- function(link='identity', link_sigma='log', link_qinv='log', link_pq='logm1', link_lambdap1half='logit'){
+constrained_sgt <- function(link='identity', link_sigma='log', link_q='log', link_pq='logm1', link_lambdap1half='logit'){
   brms::custom_family(
     name='constrained_sgt',
-    dpars=c('mu', 'sigma', 'lambdap1half', 'qinv', 'pq'),
+    dpars=c('mu', 'sigma', 'lambdap1half', 'q', 'pq'),
     lb = c(NA, 0, 0, 0, 1),
     ub = c(NA, NA, 1, NA, NA),
-    links=c(mu=link, sigma=link_sigma, lambdap1half=link_lambdap1half, q=link_qinv, pq=link_pq),
+    links=c(mu=link, sigma=link_sigma, lambdap1half=link_lambdap1half, q=link_q, pq=link_pq),
     posterior_predict = posterior_predict_constrained_sgt,
     posterior_epred = posterior_epred_constrained_sgt,
     log_lik = log_lik_constrained_sgt
@@ -21,11 +21,11 @@ constrained_sgt_default_prior <- function(params = 'all', exclude=FALSE){
     list(
       brms::prior(student_t(3, 0, 2.5), class='sigma', lb=0),
       brms::prior(beta(2,2), class='lambdap1half', lb=0, ub=1),
-      brms::prior(inv_gamma(3, .1), class='qinv', lb=0),
+      brms::prior(gamma(3, .1), class='q', lb=0),
       brms::prior(gamma(3, .1), class='pq', lb=1)
     )
 
-  which <- c('sigma', 'lambdap1half', 'qinv', 'pq')
+  which <- c('sigma', 'lambdap1half', 'q', 'pq')
   if ('all' %in% params) {
     if (exclude) stop("Are you sure?") else params <- which
   }
@@ -40,10 +40,9 @@ log_lik_constrained_sgt <- function(i, prep) {
   mu <- brms::get_dpar(prep, "mu", i = i)
   sigma <- brms::get_dpar(prep, "sigma", i=i)
   lambdap1half <- brms::get_dpar(prep, "lambdap1half", i=i)
-  qinv <- brms::get_dpar(prep, "qinv", i=i)
-  q <- 1/qinv
+  q <- brms::get_dpar(prep, "q", i=i)
   pq <- brms::get_dpar(prep, "pq", i=i)
-  p <- pq * qinv
+  p <- pq/q
   y <- prep$data$Y[i]
   mapply(sgt::dsgt,
          x=y,
@@ -60,10 +59,9 @@ posterior_predict_constrained_sgt <- function(i, prep, ...) {
   mu <- brms::get_dpar(prep, "mu", i = i)
   sigma <- brms::get_dpar(prep, "sigma", i=i)
   lambdap1half <- brms::get_dpar(prep, "lambdap1half", i=i)
-  qinv <- brms::get_dpar(prep, "qinv", i=i)
-  q <- 1/qinv
+  q <- brms::get_dpar(prep, "q", i=i)
   pq <- brms::get_dpar(prep, "pq", i=i)
-  p <- pq*qinv
+  p <- pq/q
   mapply(sgt::rsgt,
          n=1,
          mu=mu,
@@ -79,10 +77,9 @@ posterior_epred_constrained_sgt <- function(prep) {
   mu <- brms::get_dpar(prep, "mu")
   sigma <- brms::get_dpar(prep, "sigma")
   lambdap1half <- brms::get_dpar(prep, "lambdap1half")
-  qinv <- brms::get_dpar(prep, "qinv")
-  q <- 1/qinv
+  q <- brms::get_dpar(prep, "q")
   pq <- brms::get_dpar(prep, "pq")
-  p <- pq*qinv
+  p <- pq/q
   lambda <- lambdap1half * 2 - 1
   mu - (2 * sigma * lambda * q^(1/p) * beta(2/p, q - 1/p))/beta(1/p, q)
 }
